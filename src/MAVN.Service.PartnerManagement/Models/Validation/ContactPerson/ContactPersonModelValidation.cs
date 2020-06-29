3faabcc70af6a57f9b8f203f3563ef6fb6049a51
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-06-29 15:29:25 +0300

    remove blank lines

--- src/MAVN.Service.PartnerManagement/Models/Validation/ContactPerson/ContactPersonModelValidation.cs
+++ src/MAVN.Service.PartnerManagement/Models/Validation/ContactPerson/ContactPersonModelValidation.cs
@@ -26,19 +26,16 @@ namespace MAVN.Service.PartnerManagement.Models.Validation.ContactPerson
                 .WithMessage("Last Name should contains only letters")
                 .When(x => !string.IsNullOrEmpty(x.LastName));
 
-
             RuleFor(p => p.Email)
                 .EmailAddress()
                 .WithMessage("The Email should be present and within a valid email address.")
                 .When(x => !string.IsNullOrEmpty(x.Email));
-
 
             RuleFor(p => p.PhoneNumber)
                 .MinimumLength(3)
                 .MaximumLength(30)
                 .WithMessage("The Phone number should be present and within a range of 3 to 30 characters long.")
                 .When(x => !string.IsNullOrEmpty(x.PhoneNumber));
-
         }
     }
 }
